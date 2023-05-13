ca0d4969182b3603c50c6069c1522417212cbc4f
Rafael Mariano <rafael.silva@digi.ag>
2023-05-13 17:26:51 -0300

    fix: adjuste user details page

--- src/pages/user/[name].tsx
+++ src/pages/user/[name].tsx
@@ -1,10 +1,19 @@
 import ModalUser from "@/components/modalUser";
 import { userState } from "@/states/users";
 import { useRecoilValue } from "recoil";
+import { useRouter } from 'next/router';
+import { useUsers } from "@/hooks/useUsers";
+import { useEffect } from "react";
 
 export default function UserDetails() {
-    
-    //Data provisional of person.
+    const { handleGetUserByName } = useUsers();
+    const router = useRouter();
+
+    useEffect(() => {
+        if(router.query.name)
+        handleGetUserByName(router.query.name);
+      }, [router.query]);
+
     const personData = useRecoilValue(userState)
 
     return (
