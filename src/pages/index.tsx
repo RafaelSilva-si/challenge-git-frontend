17c9dd68e6004825ae31142ace408da57e2b3c82
Rafael Mariano <rafael.silva@digi.ag>
2023-05-13 14:50:55 -0300

    feat: create call for useUsers hook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,22 @@
 import CardUser from '@/components/cardUser'
+import { useUsers } from '@/hooks/useUsers'
 import { listUsersState } from '@/states/users'
+import { useEffect } from 'react';
 import { useRecoilValue } from 'recoil'
 
 export default function Home() {
+  const { handleListUsers } = useUsers();
+  const users = useRecoilValue(listUsersState);
 
-  //List provisional of Peaple.
-  const people = useRecoilValue(listUsersState)
+  useEffect(() => {
+    handleListUsers(1);
+  },[]);
 
   return (
     <main className={`flex min-h-screen flex-col items-center p-24`}>
       <h1 className='mb-2 font-semibold'>Users</h1>
       <ul role="list" className="divide-y divide-gray-100">
-        {people.map((person) => (
+        {users?.map((person) => (
           <CardUser person={person}/>
         ))}
       </ul>
